f1d30a071ea8215c561900a90acc8630b15a4d0d
jitao <jitao@jitao.tech>
2021-04-07 14:48:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# gradle-offline
+
+编译命令
+```
+./gradlew --offline clean build
+```
+
+提交前下载依赖jar
+```
+./gradlew copyToLibs
+```
---
+++ src/main/java/tech/jitao/gradleoffline/GradleOfflineApplication.java
@@ -0,0 +1,13 @@
+package tech.jitao.gradleoffline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleOfflineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleOfflineApplication.class, args);
+	}
+
+}
